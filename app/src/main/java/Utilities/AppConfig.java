34925bd21824332601f881df965570b9574019b8
shegeblaqofficials <olaosebikan4officials@gmail.com>
2017-04-20 03:06:20 +0100

    Initial commit

--- app/src/main/java/Utilities/AppConfig.java
+++ app/src/main/java/Utilities/AppConfig.java
@@ -6,8 +6,10 @@ package Utilities;
 
 public class AppConfig {
 
-    public static String PopularBaseUrl="https://api.themoviedb.org/3/movie/popular?api_key=13fcd9fb3b014841ad9b17fe5f550d3f";
-    public static String TopRateBaseUrl="https://api.themoviedb.org/3/movie/top_rated?api_key=13fcd9fb3b014841ad9b17fe5f550d3f";
+    private static String API_KEY="api_key";//// replace with api key
+
+    public static String PopularBaseUrl="https://api.themoviedb.org/3/movie/popular?api_key="+API_KEY;
+    public static String TopRateBaseUrl="https://api.themoviedb.org/3/movie/top_rated?api_key="+API_KEY;
 
     public static String PosterBaseUrl="http://image.tmdb.org/t/p/w185//";
     public static String PosterBigBaseUrl="http://image.tmdb.org/t/p/original//";
