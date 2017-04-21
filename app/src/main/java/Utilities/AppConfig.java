36bfe876351dfe4c442306d787141b69396d75fc
shegeblaqofficials <olaosebikan4officials@gmail.com>
2017-04-21 22:59:48 +0100

    Initial commit

--- app/src/main/java/Utilities/AppConfig.java
+++ app/src/main/java/Utilities/AppConfig.java
@@ -6,7 +6,7 @@ package Utilities;
 
 public class AppConfig {
 
-    private static String API_KEY="api_key";//// replace with api key
+    private static String API_KEY="13fcd9fb3b014841ad9b17fe5f550d3f";//// replace with api key
 
     public static String PopularBaseUrl="https://api.themoviedb.org/3/movie/popular?api_key="+API_KEY;
     public static String TopRateBaseUrl="https://api.themoviedb.org/3/movie/top_rated?api_key="+API_KEY;
